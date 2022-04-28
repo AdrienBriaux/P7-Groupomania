24fafcd156afa365cabc9fe18263bd1ecec72a97
AdrienBriaux <devaccess@hotmail.com>
2022-04-28 18:01:43 +0200

    test

--- backend/config/bdd.js
+++ backend/config/bdd.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 // Connection à la base de données
 
-const sequelize = new Sequelize('database', 'username', 'password', {
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
 
     host: 'localhost',
     dialect: 'mysql',
