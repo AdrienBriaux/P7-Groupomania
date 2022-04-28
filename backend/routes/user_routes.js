6d17b4e572a9d75e7e3f905c6cf9b67fa7250046
AdrienBriaux <devaccess@hotmail.com>
2022-04-28 22:21:37 +0200

    add commentaires

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -4,7 +4,13 @@ const router = express.Router();
 // On cible le controleur specifique
 const userCtrl = require('../controllers/user_controller');
 
+// Route pour créer son compte utilisateur
 router.post('/signup', userCtrl.signup);
+
+// Route pour la connection utilisateur
 router.post('/login', userCtrl.login);
+
+// Route pour supprimer son compte
+router.delete('');
 
 module.exports = router;
