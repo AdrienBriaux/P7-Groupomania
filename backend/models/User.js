a0f563b3f28834992c10735411cdc0d4d45ac3cb
AdrienBriaux <devaccess@hotmail.com>
2022-04-29 21:06:08 +0200

    schema ok

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,12 +5,12 @@ const sequelize = new Sequelize('mysql::memory:');
 
 const userSchema = sequelize.define('userSchema', {
 
-    user_id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
     admin: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
-    user_first_name: { type: DataTypes.STRING(255), allowNull: false },
-    user_last_name: { type: DataTypes.STRING(255), allowNull: false },
-    user_email: { type: DataTypes.STRING(255), allowNull: false, unique: true },
-    user_password: { type: DataTypes.STRING(255), allowNull: false }
+    first_name: { type: DataTypes.STRING(255), allowNull: false },
+    last_name: { type: DataTypes.STRING(255), allowNull: false },
+    email: { type: DataTypes.STRING(255), allowNull: false, unique: true },
+    password: { type: DataTypes.STRING(255), allowNull: false }
 },
 
     { tableName: 'user', timestamps: false, underscored: true } // Par default "tableName" serait "user" (au pluriel), "timestamps" créé 2 champs automatique pour les dates de création et de modification (très pratique si nécessaire) et "underscored" permet de créer automatiquement des champs de "relation" entre les tables de type "role_id" plutôt que "UserId".
