910ff90388cb90c6fb77cb14b1af12249d37a006
AdrienBriaux <devaccess@hotmail.com>
2022-04-29 18:36:39 +0200

    maj schema

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -8,6 +8,7 @@ const postSchema = sequelize.define('postSchema', {
     id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
     content: { type: DataTypes.STRING(200), allowNull: true },
     comment: { type: DataTypes.STRING(500), allowNull: false },
+    like: { type: DataTypes.NUMBER(100), allowNull: true, defaultValue: 0 },
 },
     { tableName: 'post', timestamps: true, underscored: true }
 );
