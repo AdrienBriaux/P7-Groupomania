d3294e0753429823b870fff215d5fcbafcaf0baf
AdrienBriaux <devaccess@hotmail.com>
2022-04-29 18:38:39 +0200

    maj schema

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -1,7 +1,7 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize('sqlite::memory:');
 
-// Création du schéma d'un like
+// Création du schéma d'un commentaire
 const commentSchema = sequelize.define('commentSchema', {
 
     id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
--- backend/models/Image.js
+++ backend/models/Image.js
@@ -1,11 +1,11 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize('sqlite::memory:');
 
-// Création du schéma d'un like
+// Création du schéma d'une image
 const imageSchema = sequelize.define('imageSchema', {
 
     id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    content: { type: DataTypes.STRING(100), allowNull: true, defaultValue: null },
+    content: { type: DataTypes.STRING(100), allowNull: false },
 },
     { tableName: 'data', timestamps: false, underscored: true }
 );
