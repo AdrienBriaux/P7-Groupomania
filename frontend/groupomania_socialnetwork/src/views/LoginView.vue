02e6840cb01328b62ac77fe6b453ae400cad350c
AdrienBriaux <devaccess@hotmail.com>
2022-05-18 15:55:13 +0200

    form work in progress

--- frontend/groupomania_socialnetwork/src/views/LoginView.vue
+++ frontend/groupomania_socialnetwork/src/views/LoginView.vue
@@ -1,12 +1,32 @@
 <template>
 
-    <p>Login</p>
+    <form @submit="handleLogin">
+
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input name="email" type="email" class="form-control" />
+        </div>
+
+        <div class="form-group">
+            <label for="password">Mot de passe</label>
+            <input name="password" type="password" class="form-control" />
+        </div>
+
+        <div class="form-group">
+            <button class="btn__login">
+                <span>Se connecter</span>
+            </button>
+        </div>
+
+    </form>
+
 </template>
 <script>
 
 
 export default {
-    name: 'LoginView',
 
+    name: 'LoginView'
 }
+
 </script>
--- frontend/groupomania_socialnetwork/src/views/RegisterView.vue
+++ frontend/groupomania_socialnetwork/src/views/RegisterView.vue
@@ -1,14 +1,39 @@
 <template>
 
-    <p>Register</p>
+    <Form @submit="handleRegister">
+
+        <div class="form-group">
+            <label for="first_name">Nom</label>
+            <input name="first_name" type="text" class="form-control" />
+        </div>
+        <div class="form-group">
+            <label for="last_name">Prénom</label>
+            <input name="last_name" type="text" class="form-control" />
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input name="email" type="email" class="form-control" />
+        </div>
+        <div class="form-group">
+            <label for="password">Mot de passe</label>
+            <input name="password" type="password" class="form-control" />
+        </div>
+
+        <div class="form-group">
+            <button class="btn__signup">
+                <span>S'enregistrer</span>
+            </button>
+        </div>
+
+    </Form>
+
 </template>
 
 <script>
-// @ is an alias to /src
-
 
 export default {
-    name: 'RegisterView',
 
+    name: 'RegisterView',
 }
+
 </script>
